bb24de7c439b5bb29fe868e0cc3e3a6eca8ab9c2
pk <pk13610@163.com>
2013-12-11 11:59:54 +0800

    update

--- httputil.py
+++ httputil.py
@@ -37,18 +37,20 @@ class HttpUtil(object):
 
     def get(self, url):
         self._init(force=True)
-        return self._request(url)
+        return self._request(url).read(self._buffer_size)
 
     def post(self, url, post_dic):
         self._init(force=True)
-        return self._request(url, post_data=urllib.urlencode(post_dic).encode(self._charset))
+        return self._request(
+            url,
+            post_data=urllib.urlencode(post_dic).encode(self._charset)
+        ).read(self._buffer_size)
 
     def fetch(self, url, handler, post_data=None):
         if handler is None:
             raise ValueError, "need handler"
         self._init(force=True)
-        self._opener.add_handler(handler)
-        self._request(url, post_data)
+        handler.handle(req=None, resp=self._request(url, post_data))
 
     def add_header(self, key, value):
         self._headers[key] = value
@@ -61,8 +63,7 @@ class HttpUtil(object):
     def _request(self, url, post_data=None):
         socket.setdefaulttimeout(self._timeout)
         req = urllib2.Request(url, data=post_data, headers=self._headers)
-        resp = self._opener.open(req)
-        return resp.read(self._buffer_size)
+        return self._opener.open(req)
 
     def _init(self, force=False):
         #if self.handler and hasattr(self.handler, 'start'):
@@ -78,27 +79,36 @@ class HttpUtil(object):
         urllib2.install_opener(self._opener)
 
 import time
-class DownloadStreamHandler(urllib2.BaseHandler):
-
-    def __init__(self, fp, duration=None, size=1400):
+import threading
+class DownloadStreamHandler:
+    """ download a http stream
+          can not do this work by urllib2.BaseHandler way,
+          I don't know how to stop download in tht middle of processing,
+          other handler maybe cause a timeout I guess.
+          so this handler works outside of urllib2.open()
+    """
+    def __init__(self, fp, duration=-1, size=1400):
         self.buff_size = size
         self.fp = fp
         self.duration = duration
-        if duration:
+        self.ev = threading.Event()
+        if duration > 0:
             self.stop_time = duration + time.time()
 
-    def http_request(self, req):
-        return req
+    def syn_stop(self):
+        self.ev.set()
 
-    def http_response(self, req, resp):
-        while self.handle(resp.read(self.buff_size)):
+    def handle(self, req, resp):
+        self.ev.clear()
+        while not self.ev.is_set() \
+            and self._handle(resp.read(self.buff_size)):
             pass
-        return resp
 
-    def handle(self, data):
+    def _handle(self, data):
         if not data: return False
         self.fp.write(data)
-        return self.duration is None or self.stop_time > time.time()
+        return self.duration == -1 \
+            or self.stop_time > time.time()
 
 import gzip
 import zlib
@@ -129,31 +139,30 @@ class ContentEncodingProcessor(urllib2.BaseHandler):
             resp.msg = old_resp.msg
         return resp
 
-def test_get():
-    url = r'http://www.twitter.com'
-    client = HttpUtil()
-    content=client.get(url)
-    print(content.decode('utf8'))
+import unittest
+class TestMethod(unittest.TestCase):
+    def test_get(self):
+        url = r'http://www.twitter.com'
+        client = HttpUtil()
+        content=client.get(url)
+        print(content.decode('utf8'))
 
-def test_post():
-    url = r'http://www.2kdy.com/search.asp'
-    post = {'searchword': r'lie'}
-    client = HttpUtil(charset='gb2312')
-    content=client.post(url, post)
-    print content 
+    def test_post(self):
+        url = r'http://www.2kdy.com/search.asp'
+        post = {'searchword': r'lie'}
+        client = HttpUtil(charset='gb2312')
+        content=client.post(url, post)
+        print content
 
-def test_fetch():
-    url = r'http://pb.hd.sohu.com.cn/stats.gif?msg=caltime&vid=772959&tvid=596204&ua=pp&isHD=21&pid=348552429&uid=13832983422211404270&out=0&playListId=5029335&nid=353924663&tc=2400&type=vrs&cateid=&userid=&uuid=779b9c99-3c3a-52bc-2622-8bb0218cad5d&isp2p=0&catcode=101&systype=0&act=&st=144792%3B6560%3B143697%3B143699&ar=10&ye=2010&ag=5%u5C81%u4EE5%u4E0B&lb=2&xuid=&passport=&fver=201311211515&url=http%3A//tv.sohu.com/20120925/n353924663.shtml&lf=http%253A%252F%252Fv.baidu.com%252Fv%253Fword%253D%2525CA%2525AE%2525D2%2525BB%2525C2%2525DE%2525BA%2525BA%2526ct%253D301989888%2526rn%253D20%2526pn%253D0%2526db%253D0%2526s%253D0%2526fbl%253D800&autoplay=1&refer=http%3A//tv.sohu.com/20120925/n353924666.shtml&t=0.24127451563254'
-    client = HttpUtil()
-    #client.set_proxy({"http":"http://127.0.0.1:8087"})
-    handle = DownloadStreamHandler(open('/Users/pk/Downloads/tmp.flv', 'w'), duration=10)
-    client.fetch(url, handle)
+    def test_fetch(self):
+        url = r'http://pb.hd.sohu.com.cn/stats.gif?msg=caltime&vid=772959&tvid=596204&ua=pp&isHD=21&pid=348552429&uid=13832983422211404270&out=0&playListId=5029335&nid=353924663&tc=2400&type=vrs&cateid=&userid=&uuid=779b9c99-3c3a-52bc-2622-8bb0218cad5d&isp2p=0&catcode=101&systype=0&act=&st=144792%3B6560%3B143697%3B143699&ar=10&ye=2010&ag=5%u5C81%u4EE5%u4E0B&lb=2&xuid=&passport=&fver=201311211515&url=http%3A//tv.sohu.com/20120925/n353924663.shtml&lf=http%253A%252F%252Fv.baidu.com%252Fv%253Fword%253D%2525CA%2525AE%2525D2%2525BB%2525C2%2525DE%2525BA%2525BA%2526ct%253D301989888%2526rn%253D20%2526pn%253D0%2526db%253D0%2526s%253D0%2526fbl%253D800&autoplay=1&refer=http%3A//tv.sohu.com/20120925/n353924666.shtml&t=0.24127451563254'
+        client = HttpUtil()
+        #client.set_proxy({"http":"http://127.0.0.1:8087"})
+        handle = DownloadStreamHandler(open('/Users/pk/Downloads/tmp.flv', 'w'), duration=10)
+        client.fetch(url, handle)
 
 if __name__ == '__main__':
     try:
-        print '=====> %s start: %s' % (__file__, time.strftime("%Y-%m-%d %X", time.localtime()))
-        test_fetch()
+        unittest.main()
     except(KeyboardInterrupt):
         print 'ctl+c'
-    finally:
-        print '=====> %s stop:  %s' % (__file__, time.strftime("%Y-%m-%d %X", time.localtime()))
